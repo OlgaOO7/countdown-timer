ac3c14fb52f6dc2e6a6f9ebe10696faaa3ed432b
Olga007 <o.piankova@ukr.net>
2023-08-31 19:35:40 +0300

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import CountdownTimer from './components/CountDownTimer/CountDownTimer';
 
 function App() {
   const targetDate = new Date('2023-09-01T09:00:00+03:00').getTime();
-  // const targetDate = new Date('2023-08-31T18:57:00+03:00').getTime();
   return (
     <div>
       <CountdownTimer targetDate={targetDate} />
